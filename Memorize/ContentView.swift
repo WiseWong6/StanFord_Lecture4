163a159e7d8a9b93b7823307f0f5673757e8ce65
Wise Wong <imhwc@icloud.com>
2024-06-16 19:54:05 +0800

    构建基础代码

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["🐶", "🐱", "🐭", "🐹", "🦊", "🐻", "🐼", "🐻‍❄️", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "🐋", "🐧", "🐦", "🐤", "🐺", "🐡"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 85),spacing:0)],spacing:0) {
+                ForEach(0 ..< emojis.count, id: \.self) { index in
+                    CardView(cardContent: emojis[index]).aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
+                }
+            }
         }
+
         .padding()
+    }
+}
+
+struct CardView: View {
+    var cardContent: String
+    @State var isFaceUp = true
+
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12.0)
+            Group {
+                base.stroke(lineWidth: 2.0)
+                base.fill(.white)
+                Text(cardContent)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }.opacity(isFaceUp ? 1:0)
+            base.opacity(isFaceUp ? 0:1)
+        }
+        .foregroundColor(.blue)
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
